7ec9151edbe4e0263dfa1ac53ac283d010708b35
AteebHussain2 <student2ateeb.hussain@gmail.com>
2025-08-15 15:34:38 +0500

    removed static files and templates from index.py

--- index.py
+++ index.py
@@ -1,9 +1,5 @@
-from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
-
 from routes.apiRoute import apiRoute
-
 from dotenv import load_dotenv
 
 
@@ -11,9 +7,6 @@ load_dotenv()
 
 
 app = FastAPI()
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
 
 app.include_router(
     apiRoute, prefix="/api/posts", tags=["create-articles-with-ai-agents"]
